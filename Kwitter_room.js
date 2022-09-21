fb11997de0dded6490861ab2376c9d4eb74a9d03
apai12148 <97473664+apai12148@users.noreply.github.com>
2022-09-20 20:55:22 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCH2kMVSGPTNG2LNzG_YoLbg9oipYQhgzQ",
+    authDomain: "classtest-58de2.firebaseapp.com",
+    projectId: "classtest-58de2",
+    storageBucket: "classtest-58de2.appspot.com",
+    messagingSenderId: "709970277497",
+    appId: "1:709970277497:web:8337e85c1717ba9d8f50a8"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
